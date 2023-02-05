e523cdc5ba840e2a91deb8ce55b61ca3a1cc2a16
Justin Jones <jus.jones@yahoo.com>
2023-02-04 19:55:15 -0500

    commit tree 148f67e28eded99d2d04b48fa3aaed3db884307c

--- lib/a_name_error.rb
+++ lib/a_name_error.rb
@@ -1,3 +1,8 @@
 "hello world"
 
-hello_world
+"hello_world"
+
+
+#Four Common Error Types 
+#Name Errors - whenever Ruby interpreter encounters a word it doesnt recognize
+#it assumes that word is the name of a variable or a method. 
